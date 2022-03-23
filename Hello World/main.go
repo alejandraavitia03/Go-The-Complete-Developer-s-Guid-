ff591e6bbfd02c55eafab33705940407784db0f8
Alejandra Avitia <aavit004@ucr.edu>
2022-03-22 17:02:34 -0700

    Lesson 1

---
+++ Hello World/main.go
@@ -0,0 +1,10 @@
+//Two types of packages:
+//	1. Exectuable: Generates a file that we can run
+//	2. Reusable: Code used as 'helpers'. Good place to put reusable logic
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
